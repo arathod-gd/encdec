9e6b30f5667c042adfd1bf5d71c833dc59aba904
arathod-gd <arathod@griddynamics.com>
2026-02-11 10:31:08 +0530

    PMD, Checkstyle, Spotbug used

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(printString());
-    }
-
-    public static String printString(){
-        return new String("Encryption Decryption Program!!");
-    }
-}
--- src/test/java/org/stage2/MainTest.java
+++ src/test/java/org/stage2/MainTest.java
@@ -2,12 +2,40 @@ package org.stage2;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
-    void encrypt() {
+    void testMain() {
+        String input = "hello\n5\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        Main.main(new String[]{});
+
+        assertEquals("mjqqt", out.toString().trim());
+    }
+
+
+    @Test
+    void encryptNormal() {
         assertEquals("finydf", Main.encrypt("aditya", 5));
+    }
+
+    @Test
+    void encryptWrap() {
+        assertEquals("abc", Main.encrypt("xyz", 3));
+    }
+
+    @Test
+    void encryptNonLowercaseIgnored() {
+        assertEquals("ABC", Main.encrypt("ABC", 5));
     }
 }
