6c055cb9b1f930dd941bd64a8bfe5abddc02fad1
arathod-gd <arathod@griddynamics.com>
2026-02-18 13:00:23 +0530

    updated Code

--- src/main/java/org/stage2/Main.java
+++ src/main/java/org/stage2/Main.java
@@ -1,7 +1,15 @@
 package org.stage2;
+
 import java.util.Scanner;
 
+/**
+ * Main class for encryption program.
+ */
 public class Main {
+
+    /**
+     * Entry point of the program.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
@@ -11,7 +19,10 @@ public class Main {
         System.out.println(encrypt(text, key));
     }
 
-    public static String encrypt(String text, int key){
+    /**
+     * Encrypts text using Caesar cipher logic.
+     */
+    public static String encrypt(String text, int key) {
         char[] chars = text.toCharArray();
 
         for (int i = 0; i < chars.length; i++) {
